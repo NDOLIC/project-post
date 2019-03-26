071a14a56f0cd6e0c663ac9b62c4e7b968967634
NDOLIC <tblaguese1@gmail.com>
2019-03-26 23:08:31 +0200

    forms

--- instapic/forms.py
+++ instapic/forms.py
@@ -1,5 +1,19 @@
+from .models import Image, Profile
 from django import forms
 
-class NewPostForm(forms.Form):
-    your_name = forms.CharField(label='First Name',max_length=30)
-    email = forms.EmailField(label='Email')
+class NewPostForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        exclude = ['user','profile', 'pub_date','likess','follow']
+
+class NewCommentForm(forms.Form):
+    comment = forms.CharField(label='Comment',max_length=600)
+
+class Profileform(forms.ModelForm):
+     class Meta:
+         model= Profile
+         exclude = ['user']
+
+   
+
+    
