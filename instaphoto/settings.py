a42bd342b067db602e95eb7d59ad057385bb28be
NDOLIC <tblaguese1@gmail.com>
2019-03-26 23:20:00 +0200

    settings

--- instaphoto/settings.py
+++ instaphoto/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+from decouple import config
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -31,6 +33,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'instapic',
+    'tinymce',
+    'bootstrap3',
+    'actstream',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
